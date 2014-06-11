66c70f6fe8c99fee9fa246d1a0cadedf0d22824d
Yong Guan <gyong9@gmail.com>
2014-06-10 22:18:39 -0700

    add recorder.js

--- js/recorder.js
+++ js/recorder.js
@@ -13,6 +13,58 @@
             el['on' + eventType] = handler;
         }
     }
-    
+
+    function up(el, stopCondition) {
+        while (el.parentNode) {
+            el = el.parentNode;
+            if (stopCondition(el)) {
+                break;
+            }
+        }
+        return el;
+    }
+
+    function getIdOrCls(el) {
+        if (el.id) {
+            return el.id;
+        } else if (el.classList) {
+            return el.className.split(' ').join('.');
+        }
+        return '';
+    }
+
+    function getCssSelectors(el) {
+        var selectorList = ['', ''],
+            selector,
+            parentEl;
+
+        selectorList[1] = getIdOrCls(el);
+        if (selectorList[1].length === 0) {
+            selector = el.nodeName;
+
+            if (selector === 'A') {
+                selector += ':contains(' + el.textContent + ')'
+            }
+            selectorList[1] = selector;
+        }
+
+        parentEl = up(el, function () {
+            return getIdOrCls(el).length > 0;
+        });
+
+        selectorList[0] = getIdOrCls(parentEl);
+
+        return selectorList.join(' ');
+    }
+
+    function record() {
+        bind(document.body, 'click', function (e) {
+            console.log(getCssSelectors(e.target));
+        });
+    }
+
+    window.recorder = {
+        record: record
+    };
 
 }());

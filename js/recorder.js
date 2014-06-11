69848c3971de83d533a4229215b6d92844bac2b6
Yong Guan <gyong9@gmail.com>
2014-06-11 07:38:36 -0700

    add more functionalities

--- js/recorder.js
+++ js/recorder.js
@@ -4,6 +4,29 @@
 (function () {
     'use strict';
 
+    var events = [
+        'click',
+        'focus',
+        'blur',
+        'dblclick',
+        'change',
+        'keyup',
+//        'keydown',
+//        'keypress',
+//        'mousedown',
+//        'mousemove',
+//        'mouseout',
+//        'mouseover',
+        'mouseup',
+        'resize',
+        'scroll',
+        'select',
+        'submit',
+        'load',
+        'unload'
+    ];
+    var recordedActions = '';
+
     function bind(el, eventType, handler) {
         if (el.addEventListener) { // DOM Level 2 browsers
             el.addEventListener(eventType, handler, false);
@@ -11,6 +34,16 @@
             el.attachEvent('on' + eventType, handler);
         } else { // ancient browsers
             el['on' + eventType] = handler;
+        }
+    }
+
+    function unbind(el, eventType, handler) {
+        if (el.removeEventListener) {
+            el.removeEventListener(eventType, handler, false);
+        } else if (el.detachEvent) {
+            el.detachEvent("on" + eventType, handler);
+        } else {
+            el["on" + eventType] = null;
         }
     }
 
@@ -26,19 +59,24 @@
 
     function getIdOrCls(el) {
         if (el.id) {
-            return el.id;
+            return '#' + el.id;
         } else if (el.classList) {
             return el.className.split(' ').join('.');
         }
         return '';
     }
 
-    function getCssSelectors(el) {
+    function getCssSelector(el) {
         var selectorList = ['', ''],
             selector,
             parentEl;
 
         selectorList[1] = getIdOrCls(el);
+
+        if (el.id) {
+            return selectorList[1];
+        }
+
         if (selectorList[1].length === 0) {
             selector = el.nodeName;
 
@@ -58,13 +96,46 @@
     }
 
     function record() {
-        bind(document.body, 'click', function (e) {
-            console.log(getCssSelectors(e.target));
-        });
+        var i = 0,
+            len = events.length;
+
+        for (; i < len; i = i + 1) {
+            bind(document.body, events[i], function (e) {
+                var cssSelector = getCssSelector(e.target),
+                    action = e.type + ' \'' + cssSelector + '\'';
+
+                recordedActions = recordedActions + action + '\n';
+                console.log(action);
+            });
+        }
+    }
+
+    // stop doesn't work in browser console yet
+    function stop() {
+        var i = 0,
+            len = events.length;
+
+        for (; i < len; i = i + 1) {
+            unbind(document.body, events[i], function () {
+            });
+        }
+    }
+
+    function getRecordedActions() {
+        return recordedActions;
+    }
+
+    function clearRecordedActions() {
+        return recordedActions = '';
     }
 
     window.recorder = {
-        record: record
+        record: record,
+        stop: stop,
+        getRecordedActions: getRecordedActions,
+        clearRecordedActions: clearRecordedActions
     };
 
 }());
+
+recorder.record();
